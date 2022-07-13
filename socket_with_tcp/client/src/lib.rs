2298d5e4392ddaf212d41b5327b23a5d67607036
Andrey Khalepov <w54661c@gmail.com>
2022-07-13 22:00:49 +1000

    done

--- socket_with_tcp/client/src/lib.rs
+++ socket_with_tcp/client/src/lib.rs
@@ -15,15 +15,7 @@ impl SocketClient {
         let msg = buf.as_slice().decrypt();
         if &msg[0..3] == b"skt" {
             let state = msg[3];
-            let mut pwr_buf:[u8;8] = [0;8];
-            pwr_buf[0] = msg[4];
-            pwr_buf[1] = msg[5];
-            pwr_buf[2] = msg[6];
-            pwr_buf[3] = msg[7];
-            pwr_buf[4] = msg[8];
-            pwr_buf[5] = msg[9];
-            pwr_buf[6] = msg[10];
-            pwr_buf[7] = msg[11];
+            let pwr_buf:[u8;8] = msg[4..].try_into().unwrap();
             let pwr = f64::from_be_bytes(pwr_buf);
             Ok(format!("Socket state: {}, power: {:.2}", if state == 0 {"OFF"} else {"ON"}, pwr))
         } else {
