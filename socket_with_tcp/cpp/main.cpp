dc86fc7061bc3931daab6b91932d90d388027884
Andrey Khalepov <w54661c@gmail.com>
2022-09-10 20:23:03 +1000

    Fix messages

--- socket_with_tcp/cpp/main.cpp
+++ socket_with_tcp/cpp/main.cpp
@@ -6,19 +6,19 @@ using namespace std;
 int main()
 {
     int result_on = switch_on();
-    cout << "Switch on result: " << result_on << endl;
+    cout << "Switch on result: " << (result_on == 0 ? "Success" : "Failed") << endl;
 
     SocketState s = sync();
 
     if (s.error == 0) {
-        cout << "Switch state: " << s.state << endl;
+        cout << "Switch state: " << (s.state == 1 ? "On" : "Off") << endl;
         cout << "Switch power: " << s.power << endl;
     } else {
         cout << "Failed to get state" << endl;
     }
 
     int result_off = switch_off();
-    cout << "Switch off result: " << result_off << endl;
+    cout << "Switch off result: " << (result_off == 0 ? "Success" : "Failed") << endl;
 
     return 0;
 }
